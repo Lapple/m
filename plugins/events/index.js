1cf32ae156afc998883e1a707a32c5ca7eddc2e3
lapple <lapple@yandex-team.ru>
2014-04-23 18:58:59 +0400

    Wrap reset method

--- plugins/events/index.js
+++ plugins/events/index.js
@@ -2,8 +2,13 @@ var _ = require('underscore');
 var m = require('../../m');
 var Events = require('./events');
 
-var Model = m.Model;
-var Collection = m.Collection;
+function isModel(obj) {
+    return obj instanceof m.Model;
+}
+
+function isCollection(obj) {
+    return obj instanceof m.Collection;
+}
 
 module.exports = function(Class) {
     _.extend(Class.prototype, Events, {
@@ -29,7 +34,7 @@ module.exports = function(Class) {
     Class.wrap('initialize', function(fn) {
         fn.apply(this, _.tail(arguments));
 
-        if (this instanceof Collection) {
+        if (isCollection(this)) {
             this.on('add', this._onModelAdded);
             this.on('remove', this._onModelRemoved);
         }
@@ -38,7 +43,7 @@ module.exports = function(Class) {
     Class.wrap('attr', function(fn, key, value) {
         fn.apply(this, _.tail(arguments));
 
-        if (this instanceof Model) {
+        if (isModel(this)) {
             this.trigger('change:' + key, this, value);
             this.trigger('change', this);
         }
@@ -47,7 +52,7 @@ module.exports = function(Class) {
     Class.wrap('put', function(fn, model, index) {
         fn.apply(this, _.tail(arguments));
 
-        if (this instanceof Collection) {
+        if (isCollection(this)) {
             this.trigger('add', model, this, index);
         }
     });
@@ -55,9 +60,21 @@ module.exports = function(Class) {
     Class.wrap('remove', function(fn) {
         var model = fn.apply(this, _.tail(arguments));
 
-        if (model && this instanceof Collection) {
+        if (isCollection(this)) {
             this.trigger('remove', model, this);
             return model;
+        }
+    });
+
+    Class.wrap('reset', function(fn) {
+        fn.apply(this, _.tail(arguments));
+
+        if (isModel(this)) {
+            this.trigger('change', this);
+        }
+
+        if (isCollection(this) || isModel(this)) {
+            this.trigger('reset', this);
         }
     });
 };
